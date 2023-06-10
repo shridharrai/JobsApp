6fa59212440b8c171f096278f315f5881f803807
shridharrai <shridharrai3@gmail.com>
2023-06-10 21:22:23 +0530

    feat(): Inital screens setup done

---
+++ app/index.js
@@ -0,0 +1,39 @@
+import { SafeAreaView, View } from "react-native";
+import { COLORS, SIZES } from "../constants/theme";
+import { Stack } from "expo-router";
+import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import { icons, images } from "../constants";
+import { ScrollView } from "react-native";
+import Welcome from "../components/home/welcome/Welcome";
+import Popularjobs from "../components/home/popular/Popularjobs";
+import Nearbyjobs from "../components/home/nearby/Nearbyjobs";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/MyPic.jpeg";
+
+export default {
+  profile,
+};
